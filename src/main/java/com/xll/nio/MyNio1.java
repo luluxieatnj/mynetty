70ada2e3b77a75b479ec9cb5ecd6884a7e88a9b3
xielulu <xiell3@asiainfo.com>
2020-04-21 20:35:44 +0800

    asdfgh

---
+++ src/main/java/com/xll/nio/MyNio1.java
@@ -0,0 +1,36 @@
+package com.xll.nio;
+
+import java.nio.IntBuffer;
+import java.security.SecureRandom;
+
+/**
+ *  Buffer内部维护了4个指针。
+ *  // Invariants: mark <= position <= limit <= capacity
+ *     private int mark = -1;   // 标记位
+ *     private int position = 0;// 位置指针
+ *     private int limit;       // 限制位
+ *     private int capacity;
+ *
+ *     // 容量
+ */
+public class MyNio1 {
+    public static void main(String[] args) {
+        IntBuffer buffer = IntBuffer.allocate(10);
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < 10; i++) {
+            buffer.put(random.nextInt(20));
+        }
+
+        System.out.println(buffer);
+
+        // 从写模式 切换到 读模式
+        buffer.flip();
+        System.out.println(buffer);
+
+        while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
+        System.out.println(buffer);
+    }
+}
